ad6a3f6963448b5a01581d8fdf4a41f4f0fe3607
nurfatinnblh14 <47810583+nurfatinnblh14@users.noreply.github.com>
2019-08-29 09:51:19 +0800

    Add files via upload

---
+++ version 1/purata.php
@@ -0,0 +1,21 @@
+<?php
+$f = fopen("Nombor.txt", "r");
+while (!feof ($f))
+ {
+$arrNombor = explode(',', fgets($f));
+
+}
+$bilangan = count ($arrNombor);
+$jumlah = 0;
+for ($x = 0; $x < $bilangan; $x++)
+ { 
+	print $arrNombor [$x] ."<br>";
+	$jumlah = $jumlah + $arrNombor[$x];
+
+}
+$purata = $jumlah/$bilangan;
+print "Jumlah = ".$jumlah ."<br>";
+print "Nilai Purata = ".$purata."<br>";
+
+fclose ($f);
+?>
